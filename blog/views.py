06d1c612101277a4f8c06be34734a0007c6bd625
vacyny <k@lomati.ru>
2020-11-19 18:09:17 +0300

    Использование представлений на основе классов

--- blog/views.py
+++ blog/views.py
@@ -3,6 +3,8 @@ from django.shortcuts import render
 from django.shortcuts import render, get_object_or_404
 from .models import Post
 from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
+from django.views.generic import ListView
+
 
 def post_list(request):
     object_list = Post.published.all()
@@ -26,3 +28,9 @@ def post_detail(request, year, month, day, post):
         publish__month=month,
         publish__day=day)
     return render(request, 'blog/post/detail.html', {'post': post})
+
+class PostListView(ListView):
+    queryset = Post.published.all()
+    context_object_name = 'posts'
+    paginate_by = 3
+    template_name = 'blog/post/list.html'
